f89b46e49b83c3ae691c0e06794f6d9f4eba3161
TSephanya Brun <tsbrun@icloud.com>
2022-04-25 17:55:40 -0400

    Plan gameplay component

---
+++ src/components/Hangman.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hangman() {
+    return(
+        <>
+        <h1>Hangman</h1>
+        {/* img of hangman */}
+        {/* hint for word */}
+        {/* display for correct keystrokes */}
+        </>
+    )
+}
+
+export default Hangman;
