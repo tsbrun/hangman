4b41d1c4d2154907477abc9f5796d2b5e6f60def
TSephanya Brun <tsbrun@icloud.com>
2022-04-28 13:20:50 -0400

    Fetch level 1 data

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "./Image";
 import Hint from "./Hint";
 import Word from "./Word";
 
 function Hangman() {
+    const [level, setLevel] = useState("")
+
+    useEffect(() => {
+        const url = "http://localhost:3001/levels/1"
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url)
+                const json = await response.json()
+                // console.log('i fire once')
+                // console.log(json)
+                setLevel(json)
+            } catch (error) {
+                console.log("error", error)
+            }
+        }
+
+        fetchData()
+        // console.log(level)
+    }, [])
+
+    
     return(
         <>
         <h1>Hangman</h1>
         <Image />
-        <Hint />
+        <Hint hint={level.render.hint} />
         <Word />
         </>
     )
