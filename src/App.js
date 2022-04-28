aa6e4a9dfec65ef23844d54348496723479a925e
TSephanya Brun <tsbrun@icloud.com>
2022-04-28 18:56:50 -0400

    Update app container with new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Home from './components/Home';
+import Levels from './components/Levels';
+import ErrorBoundary from './components/ErrorBoundary';
 import Hangman from './components/Hangman';
 
 function App() {
   return (
     <div className="App">
-      <Hangman />
+      <h1>Hangman</h1>
+      <Home />
+      <Levels />
+      {/* <ErrorBoundary> */}
+        <Hangman />
+      {/* </ErrorBoundary> */}
     </div>
   );
 }
