5359d90ceaad724e0aaa4fe4a4457c149863242c
TSephanya Brun <tsbrun@icloud.com>
2022-05-10 18:05:00 -0400

    React to user input based on whether it matches correct answer

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,24 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function Letter({ key, answer }) {
-    const [letter, setLetter] = useState('')
-
-    console.log(key, answer)
-
-    function handleOnKeyDown(e) {
-        console.log(e.key)
+class Letter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            key: this.props.key,
+            answer: this.props.answer,
+            input: ''
+        }
     }
 
-    return(
-        <>
-        <input 
-        className="hangman-input" 
-        type="text" 
-        value={letter} 
-        onKeyDown={handleOnKeyDown} 
-        onChange={ (e) => setLetter(e.target.value) }
-        >
-        </input>
-        </>
-    )
+    handleOnKeyDown = (e) => {
+        // if (e.key === /^[a-zA-Z]*$/) {
+            if (this.state.answer === e.key) {
+                console.log('yay, green box')
+            } else if (this.state.answer !== e.key) {
+                console.log('whoops, red box')
+            }
+        // }
+    }
+
+    render() {
+        return (
+            <>
+            <input 
+            className="hangman-input" 
+            type="text" 
+            value={this.state.letter} 
+            onKeyDown={this.handleOnKeyDown} 
+            onChange={ (e) => this.setState({input: e.target.value}) }
+            >
+            </input>
+            </>
+        )
+    }
 }
+
+export default Letter;
