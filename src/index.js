b5da591d5290b3954811c82c126f3ddc97f6fdfe
TSephanya Brun <tsbrun@icloud.com>
2022-05-13 17:50:11 -0400

    Create store and make it available to components

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LevelsRoute from './routes/levels';
 import HangmanRoute from './routes/hangman';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider>
+    <BrowserRouter store={store}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/levels" element={<LevelsRoute />} />
@@ -25,6 +28,7 @@ root.render(
         />
       </Routes> 
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
