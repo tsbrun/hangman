0e47064c282692f84efea19ac7a15874d000ee8d
TSephanya Brun <tsbrun@icloud.com>
2022-05-17 13:51:40 -0400

    Load levels into store in topmost container component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import Home from './components/Home';
 
 import { Link } from "react-router-dom";
 
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchLevels, levelsSelector } from './features/levels/levelsSlice';
+
 function App() {
+  // initialize redux hook
+  const dispatch = useDispatch()
+  const { levels, loading, hasErrors } = useSelector(levelsSelector)
+
+
+  // dispatch thunk when component first mounts
+  useEffect(() => {
+      dispatch(fetchLevels())
+  }, [dispatch])
+  
+  console.log(levels)
+
   return (
     <div className="App">
       <h1>Hangman</h1>
       <Home />
 
-      <Link to="/levels">
+      <h2>Let's Get Started</h2>
+
+      {/* <Levels /> */}
+
+      {/* <Link to="/levels">
         <button type="button">
           Let's Get Started
         </button>
-      </Link>
+      </Link> */}
     </div>
   );
 }
