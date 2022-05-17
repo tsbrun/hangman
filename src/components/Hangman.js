f9a6374070135037b6b2434d9a29961ace488567
TSephanya Brun <tsbrun@icloud.com>
2022-05-17 14:14:17 -0400

    Access data passed down from Link

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -3,21 +3,13 @@ import Image from "./Image";
 import Hint from "./Hint";
 import Word from "./Word";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { fetchLevels, levelsSelector } from "../features/levels/levelsSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function Hangman({ id }) {
-    // initialize redux hook
-    const dispatch = useDispatch()
-    const { levels, loading, hasErrors } = useSelector(levelsSelector)
-
-
-    // dispatch thunk when component first mounts
-    useEffect(() => {
-        dispatch(fetchLevels())
-    }, [dispatch])
-
-    const level = levels[id.id]
+    const location = useLocation()
+    const { level } = location.state
     
     return(
         <>
