09c9745568ed56eb7b864666b2aede6cfa9bc11a
TSephanya Brun <tsbrun@icloud.com>
2022-05-18 15:43:35 -0400

    Bring Levels container up higher in component tree

--- src/containers/Levels.js
+++ src/containers/Levels.js
@@ -3,13 +3,14 @@ import Level from "../components/levels/Level";
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 
-function Levels() {
-    const location = useLocation()
-    const { levels } = location.state
+function Levels(props) {
+    // const location = useLocation()
+    // const { levels } = location.state
+    const levels = props.data
 
     return(
         <>
-        <Link to="/">Home</Link>
+        {/* <Link to="/">Home</Link> */}
         
         <div className="levels-container">
             {levels.map(x => (
