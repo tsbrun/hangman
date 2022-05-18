121cee3b907e3711b78ecc5584232b1a611cac1a
TSephanya Brun <tsbrun@icloud.com>
2022-05-18 16:24:23 -0400

    Set up local state

--- src/containers/Hangman.js
+++ src/containers/Hangman.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "../components/hangman/Image";
 import Hint from "../components/hangman/Hint";
 import Word from "../components/hangman/Word";
@@ -6,16 +6,25 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 function Hangman() {
+    // get level from location object
     const location = useLocation()
     const { level } = location.state
+
+    // split word into letters
+    const word = level.word.split("")
+
+    // set up initial state
+    const [guesses, setGuesses] = useState([])
+    const [counter, setCounter] = useState(8)
     
     return(
         <>
-        <Link to="/">Home</Link> | <Link to="/levels">Levels</Link>
+        <Link to="/">Home</Link>   
+        {/* | <Link to="/levels">Levels</Link> */}
 
         <Image />
         <Hint hint={level.hint} />
-        <Word word={level.word} />
+        {/* <Word word={level.word} /> */}
         </>
     )
 }
