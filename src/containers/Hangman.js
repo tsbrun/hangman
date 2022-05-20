5cc23c9be62d0d02a8a30f8c5641e534ae0c95f4
TSephanya Brun <tsbrun@icloud.com>
2022-05-19 21:20:50 -0400

    Delete dead code

--- src/containers/Hangman.js
+++ src/containers/Hangman.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import Image from "../components/hangman/Image";
 import Hint from "../components/hangman/Hint";
-import Word from "../components/hangman/Word";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
 
 function Hangman() {
     // get level from location object
@@ -43,11 +43,9 @@ function Hangman() {
     return(
         <>
         <Link to="/">Home</Link>   
-        {/* | <Link to="/levels">Levels</Link> */}
 
         <Image />
         <Hint hint={level.hint} />
-        {/* <Word word={level.word} /> */}
 
         <div className="word-container">
             {alphabet.map(letter => <button onClick={e => checkGuess(e)}>{letter}</button>)}
