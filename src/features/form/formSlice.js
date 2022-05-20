d64ec93e45f19d1d1691e7f6b3dfde34e0f7396e
TSephanya Brun <tsbrun@icloud.com>
2022-05-19 21:53:49 -0400

    Prep slice and store for form refactor

---
+++ src/features/form/formSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {}
+
+export const formSlice = createSlice({
+    name: 'form', 
+    initialState,
+    reducers: {
+
+    }
+})
+
+// generate actions 
+
+// create selector(s)
+
+export default formSlice.reducer
+
+// do we need thunks?
