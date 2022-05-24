26136feb7ad938dbf61e1abfff5225a45711086c
TSephanya Brun <tsbrun@icloud.com>
2022-05-24 14:47:49 -0400

    Configure store with form reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import levelsReducer from '../features/levels/levelsSlice'
-// import formReducer from '../features/form/formSlice'
+import formReducer from '../features/form/formSlice'
 
 export const store = configureStore({
     reducer: {
         levels: levelsReducer,
-        // form: formReducer,
+        form: formReducer,
     },
 })
